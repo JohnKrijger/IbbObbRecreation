07b21ef0126d15653b290e1b36eb20ab54f7f233
John Krijger <j.krijger@students.uu.nl>
2022-10-25 20:46:23 +0200

    Reverse gravity when crossing floor object without collision

---
+++ UnityAssignment/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class count the number of ground colliders it currently overlaps with,
+// if this is zero, the character is not grounded.
+[RequireComponent(typeof(Collider))]
+public class GroundCheck : MonoBehaviour
+{
+    private int groundCount = 0;
+
+    public bool IsGrounded => groundCount > 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Floor") || other.CompareTag("Player"))
+        {
+            groundCount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Floor") || other.CompareTag("Player"))
+        {
+            groundCount--;
+        }
+    }
+}
--- UnityAssignment/Assets/Scripts/PlayerController.cs
+++ UnityAssignment/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,27 @@ public class PlayerController : MonoBehaviour
     public Collider Collider { get; private set; }
     public Rigidbody Rigidbody { get; private set; }
 
+    public GroundCheck groundCheck;
     // Input keys
     [SerializeField]
     KeyCode leftKey, rightKey, jumpKey;
     // Movement values
     [SerializeField]
     float moveSpeed = 2.5f, jumpSpeed = 5f;
+    [SerializeField]
+    float gravityScale = Physics.gravity.magnitude;
+    Vector3 gravityDirection = Vector3.down;
 
     Vector2 movement = Vector2.zero;
+    LayerMask geometryLayerMask;
 
     // Start is called before the first frame update
     void Start()
     {
         Collider = GetComponent<Collider>();
         Rigidbody = GetComponent<Rigidbody>();
+        gravityScale = Physics.gravity.magnitude;
+        geometryLayerMask = LayerMask.GetMask("Geometry");
     }
 
     // Update is called once per frame
@@ -34,11 +41,45 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(rightKey)) { movement.x += moveSpeed; }
 
         // Get vertical movement
-        movement.y = Input.GetKeyDown(jumpKey) ? jumpSpeed : Rigidbody.velocity.y;
+        if (groundCheck.IsGrounded && Input.GetKeyDown(jumpKey))
+        {
+            movement.y = -gravityDirection.y * jumpSpeed;
+        }
+        else
+        {
+            movement.y = Rigidbody.velocity.y;
+        }
     }
 
     private void FixedUpdate()
     {
+        // Flip gravity if there is no floor "beneath" the player.
+        bool flipGravity = true;
+        if (Physics.Raycast(
+            transform.position,
+            gravityDirection,
+            out RaycastHit hit,
+            Mathf.Infinity,
+            geometryLayerMask,
+            QueryTriggerInteraction.Collide
+        ) && hit.collider.CompareTag("Floor"))
+        {
+            flipGravity = false;
+        }
+        if (flipGravity)
+        {
+            // Flip the player and their gravity
+            gravityDirection *= -1f;
+            transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+            // Ensure that the player has enough vertical momentum after a flip to escape a new flip
+            if (-movement.y / gravityDirection.y < jumpSpeed)
+            {
+                movement.y = -gravityDirection.y * jumpSpeed;
+            }
+        }
+        // Apply gravity manually
+        Rigidbody.AddForce(gravityScale * gravityDirection, ForceMode.Acceleration);
+
         // Apply movement
         Rigidbody.velocity = movement;
     }
