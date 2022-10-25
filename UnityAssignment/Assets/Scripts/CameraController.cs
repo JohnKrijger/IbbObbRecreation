c26de76cbfaee4595a4dc84e3d8ca395e5ba989a
John Krijger <j.krijger@students.uu.nl>
2022-10-25 20:45:28 +0200

    Add smooth camera movement

--- UnityAssignment/Assets/Scripts/CameraController.cs
+++ UnityAssignment/Assets/Scripts/CameraController.cs
@@ -8,11 +8,21 @@ public class CameraController : MonoBehaviour
     public Camera Camera { get; private set; }
 
     public Transform ibb, obb;
+    [SerializeField]
+    [Tooltip("The distance between the players will be at most this ratio of screen space.")]
+    float focusRatio = 0.8f;
+    [SerializeField]
+    [Tooltip("How smooth the camera refocusses on the players.")]
+    float smoothingRate = 0.9f;
+
+    Vector3 smoothIbbPosition, smoothObbPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         Camera = GetComponent<Camera>();
+        smoothIbbPosition = ibb.position;
+        smoothObbPosition = obb.position;
     }
 
     // Update is called once per frame
@@ -20,18 +30,26 @@ public class CameraController : MonoBehaviour
     {
         // Center camera between players
         transform.position = new Vector3(
-            (ibb.position.x + obb.position.x) / 2f,
-            (ibb.position.y + obb.position.y) / 2f,
+            (smoothIbbPosition.x + smoothObbPosition.x) / 2f,
+            (smoothIbbPosition.y + smoothObbPosition.y) / 2f,
             transform.position.z
         );
 
-        // Zoom to keep the screen distance between players at most 10/12th of screen width or height
-        Vector3 ibbScreenPosition = Camera.WorldToScreenPoint(ibb.position);
-        Vector3 obbScreenPosition = Camera.WorldToScreenPoint(obb.position);
+        // Zoom to keep the screen distance between players at most the given ratio of screen width or height
+        Vector3 ibbScreenPosition = Camera.WorldToScreenPoint(smoothIbbPosition);
+        Vector3 obbScreenPosition = Camera.WorldToScreenPoint(smoothObbPosition);
         float xDifference = Mathf.Abs(ibbScreenPosition.x - obbScreenPosition.x) / Camera.pixelWidth;
         float yDifference = Mathf.Abs(ibbScreenPosition.y - obbScreenPosition.y) / Camera.pixelHeight;
         float maxDifference = Mathf.Max(xDifference, yDifference);
-        Camera.orthographicSize *= maxDifference * 1.2f;
+        Camera.orthographicSize *= maxDifference / focusRatio;
         Camera.orthographicSize = Mathf.Max(Camera.orthographicSize, 3f);
+    }
+
+    private void FixedUpdate()
+    {
+        smoothIbbPosition *= smoothingRate;
+        smoothObbPosition *= smoothingRate;
+        smoothIbbPosition += (1f - smoothingRate) * ibb.position;
+        smoothObbPosition += (1f - smoothingRate) * obb.position;
     }
 }
